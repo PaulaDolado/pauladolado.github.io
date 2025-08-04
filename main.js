fb11e1e6054e33227d46c8948b37b5869a0d2ae0
PaulaDolado <pauladolado3@gmail.com>
2025-08-04 23:10:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -12,11 +12,12 @@ tl.from(splitText.chars,{
     opacity:0,
     color:"#FFFFFF",
     stagger:0.05,
+    duration:0.03,
     transformOrigin:"center top",
     perspective:700,
 }).to(splitText.chars,{
     color:"#6c5ce7",
-    duration:0.3,
+    duration:0.03,
     stagger:0.05,
     ease:"power2.out",
 });
